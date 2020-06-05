ea65114948885a802c0bfe08e8e14e369da093df
steffanturanjanin <stefan@turanjanin.net>
2020-06-05 15:53:47 +0200

    Update php version

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
 Route::get('/{shorten_url}', 'UrlController@redirection')->name('redirection');
 Route::post('/url', 'UrlController@store')->name('store');
 Route::get('/details/{shorten_url}', 'UrlController@index')->name('details');
